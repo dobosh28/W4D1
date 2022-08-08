5a31d58ed291898bb2a032a99b51faf5b4ed3071
Frederick <fred.chien3@gmail.com>
2022-08-08 13:56:29 -0400

    complete winning node and supercomputer

--- tictactoe/lib/super_computer_player.rb
+++ tictactoe/lib/super_computer_player.rb
@@ -2,6 +2,23 @@ require_relative 'tic_tac_toe_node'
 
 class SuperComputerPlayer < ComputerPlayer
   def move(game, mark)
+
+    node = TicTacToeNode.new(game.board, mark, nil)
+
+    node.children.each do |child|
+      if child.winning_node?(mark)
+        return child.prev_move_pos
+      end
+    end
+
+    node.children.each do |child|
+      if child.losing_node?(mark) == false
+        return child.prev_move_pos
+      end
+    end
+
+    raise "can't find winning or draw inducing move"
+    nil
   end
 end
 
--- tictactoe/lib/tic_tac_toe_node.rb
+++ tictactoe/lib/tic_tac_toe_node.rb
@@ -9,7 +9,13 @@ class TicTacToeNode
     @prev_move_pos = prev_move_pos
   end
 
-  def losing_node?(evaluator)
+  def losing_node?(evaluator) # evaluator is "us" for now
+    if evaluator == :x
+      opponent = :o
+    else
+      opponent = :x
+    end
+
     if @board.over? 
       if @board.winner == evaluator || @board.winner.nil?
         return false
@@ -18,16 +24,33 @@ class TicTacToeNode
       end
     end
 
-    if evaluator == :o
-      self.children.all? { |child| child.losing_node?(evaluator) }
-    else
-      self.children.any? { |child| child.losing_node?(evaluator) }
+    if @next_mover_mark == evaluator # currently player's turn
+      return self.children.all? {|child| child.losing_node?(evaluator)}
+    elsif @next_mover_mark == opponent # opponent's turn
+      return self.children.any? {|child| child.losing_node?(evaluator)}
     end
   end
- # :x if human player :o is computer
 
   def winning_node?(evaluator)
-    
+    if evaluator == :x
+      opponent = :o
+    else
+      opponent = :x
+    end
+
+    if @board.over?
+      if @board.winner == evaluator
+        return true
+      else
+        return false
+      end
+    end
+
+    if @next_mover_mark == evaluator # currently player's turn
+      return self.children.any? {|child| child.winning_node?(evaluator)}
+    elsif @next_mover_mark == opponent # opponent's turn
+      return self.children.all? {|child| child.winning_node?(evaluator)}
+    end
   end
 
   # This method generates an array of all moves that can be made after
